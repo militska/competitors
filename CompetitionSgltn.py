2e057d9f928755a4a9d197fe73a8b2a10055b79d
jane <cheshirenok@gmail.com>
2018-10-01 23:59:59 +0500

    rm comments

--- CompetitionSgltn.py
+++ CompetitionSgltn.py
@@ -4,9 +4,7 @@ from Competition import Competition
 class CompetitionsSgltn:
     instance = None
 
-    def get_instance(self, n):
-        print("line 14; param " + str(n))
+    def get_instance(self):
         if self.instance is None:
-            print("line 16; param " + str(n))
             self.instance = Competition()
         return self.instance
--- Run.py
+++ Run.py
@@ -2,11 +2,11 @@ from CompetitionSgltn import CompetitionsSgltn
 
 competitions = CompetitionsSgltn()
 
-round1 = competitions.get_instance(1)
+round1 = competitions.get_instance()
 print(round1.start(12))
 
-round2 = competitions.get_instance(2)
+round2 = competitions.get_instance()
 print(round2.start(16))
 
-round3 = competitions.get_instance(3)
+round3 = competitions.get_instance()
 print(round3.start(30))
